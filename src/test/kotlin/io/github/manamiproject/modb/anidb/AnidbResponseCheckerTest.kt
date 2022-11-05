3be4a790594ecce6b3ac5088bde7df035dcfe55e
manami-project <13179375+manami-project@users.noreply.github.com>
2022-11-05 10:23:29 +0100

    Replaced junit Test annotation with kotlin Test annotation

--- src/test/kotlin/io/github/manamiproject/modb/anidb/AnidbResponseCheckerTest.kt
+++ src/test/kotlin/io/github/manamiproject/modb/anidb/AnidbResponseCheckerTest.kt
@@ -5,7 +5,7 @@ import io.github.manamiproject.modb.test.loadTestResource
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
+import kotlin.test.Test
 
 internal class AnidbResponseCheckerTest {
 
